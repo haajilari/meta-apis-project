8c786c84db605aabb68335a79baf63e831517366
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-07-01 07:24:06 +0330

    tokenize test!

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('categories', views.CategoryListCreateView.as_view()),
 ]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    ordering_fields = ['id', 'title']
+    search_fields = ['title']
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
