08c1378019b3974d876c95ff25c6ad3bf2816298
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-07-01 07:16:23 +0330

    serializer

---
+++ LittleLemonAPI/serializers.py
@@ -0,0 +1,70 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Category, MenuItem, Cart, Order, OrderItem
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'slug', 'title']
+
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all()
+    )
+
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'title', 'price', 'category', 'featured']
+
+
+class CartSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    unit_price = serializers.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        read_only=True
+    )
+    price = serializers.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        read_only=True
+    )
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'menuitem', 'quantity', 'unit_price', 'price']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'order', 'menuitem', 'quantity', 'unit_price', 'price']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = serializers.SerializerMethodField()
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = [
+            'id',
+            'user',
+            'delivery_crew',
+            'status',
+            'total',
+            'date',
+            'order_items',
+        ]
+
+    def get_order_items(self, obj):
+        items = OrderItem.objects.filter(order=obj)
+        return OrderItemSerializer(items, many=True).data
