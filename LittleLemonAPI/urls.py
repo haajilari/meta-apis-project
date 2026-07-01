86516a638a3016bbe57d0130a9e73e10055d7844
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-07-01 07:34:13 +0330

    menu-items added

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('categories', views.CategoryListCreateView.as_view()),
+
+    path('menu-items', views.MenuItemListCreateView.as_view()),
+    path('menu-items/<int:pk>', views.MenuItemDetailView.as_view()),
 ]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, MenuItem
+from .serializers import CategorySerializer, MenuItemSerializer
+from .permissions import is_manager
 
 
 class CategoryListCreateView(generics.ListCreateAPIView):
@@ -14,3 +15,60 @@ class CategoryListCreateView(generics.ListCreateAPIView):
         if self.request.method == 'POST':
             return [IsAdminUser()]
         return [IsAuthenticated()]
+
+
+class MenuItemListCreateView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    filterset_fields = ['category', 'featured']
+    ordering_fields = ['price', 'title']
+    search_fields = ['title', 'category__title']
+
+    def get_permissions(self):
+        return [IsAuthenticated()]
+
+    def post(self, request, *args, **kwargs):
+        if not is_manager(request.user):
+            self.permission_denied(
+                request,
+                message='You are not authorized to add menu items.'
+            )
+        return super().post(request, *args, **kwargs)
+
+
+class MenuItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+    def get_permissions(self):
+        return [IsAuthenticated()]
+
+    def put(self, request, *args, **kwargs):
+        if not is_manager(request.user):
+            self.permission_denied(
+                request,
+                message='You are not authorized to update menu items.'
+            )
+        return super().put(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        if not is_manager(request.user):
+            self.permission_denied(
+                request,
+                message='You are not authorized to update menu items.'
+            )
+        return super().patch(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        if not is_manager(request.user):
+            self.permission_denied(
+                request,
+                message='You are not authorized to delete menu items.'
+            )
+        return super().delete(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.permission_denied(
+            request,
+            message='POST is not allowed on a single menu item.'
+        )
